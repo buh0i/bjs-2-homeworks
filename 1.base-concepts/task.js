3505304a8fdb65ea4250c4df2f82cac375414f10
Anton <buh000i@yandex.ru>
2022-09-12 18:08:19 +0400

    first task commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,59 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  "use strict"
+  let arr = [];
+  let d = ((b**2)-(4*a*c));
+  if (d === 0) {
+    arr.push(-b/(2*a));
+  } else if (d > 0) {
+    arr.push((-b + Math.sqrt(d))/(2*a))
+    arr.push((-b - Math.sqrt(d))/(2*a))
+  } else {
+    arr = [];
+  } 
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+  "use strict"
+
+  percent = +percent;
+  contribution = +contribution;
+  amount = +amount;
+
   let totalAmount;
+  let bodyOfCredit = amount - contribution;
+  let _MS_PER_MONTH = 1000 * 60 * 60 * 24 * 30;
+  let nowDate = new Date();
+  let remainingMonths;
+  let monthlyPayment;
+  let truePercent = (percent / 100);
+  
+  console.log (typeof percent, percent)
 
-  // код для задачи №2 писать здесь
+  if (typeof percent !== "number") {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"` 
+  } else if (typeof contribution !== "number") {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`
+  } else if (typeof amount !== "number") {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`
+  } else {
+    console.log(bodyOfCredit, nowDate, _MS_PER_MONTH);
 
-  return totalAmount;
+    function dateDiffInDays(nowDate, date) {
+      let utc1 = Date.UTC(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate());
+      let utc2 = Date.UTC(date.getFullYear(), date.getMonth(), date.getDate());
+      console.log(utc1, utc2);
+      return Math.floor((utc2 - utc1) / _MS_PER_MONTH);
+    }
+  
+    remainingMonths = dateDiffInDays(nowDate, date);
+  
+    monthlyPayment = bodyOfCredit * ((truePercent/12) + ((truePercent/12) / (((1 + (truePercent/12))**remainingMonths) - 1)));
+  
+    totalAmount = monthlyPayment * remainingMonths;
+  
+    console.log("Сколько месяцев платить: " + +remainingMonths + " Ежемесячный платеж: " + +monthlyPayment.toFixed(2) + " Сколько всего платить: " + +totalAmount.toFixed(2));
+  }
+
+  return totalAmount.toFixed(2);
 }
