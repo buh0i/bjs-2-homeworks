f956623773fcd6eacca2c9973ec7d87e63550d08
Anton <buh000i@yandex.ru>
2022-09-14 19:01:28 +0400

    third task commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  result = arr1.length === arr2.length && arr1.every((elem, i) => elem === arr2[i]);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter((elem) => elem > 0 && elem % 3 === 0).map((elem) => elem * 10);
   return resultArr; // array
 }
